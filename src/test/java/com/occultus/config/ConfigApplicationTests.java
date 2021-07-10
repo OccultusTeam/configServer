f31e8e21c9e5f567b0fa25e508dfd0108638de60
occutlus <s8888456456@hotmail.com.tw>
2021-07-10 16:31:52 +0800

    init project v1

---
+++ src/test/java/com/occultus/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.occultus.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
